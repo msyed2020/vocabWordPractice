730f08603f9c7fee28fd94518ce73046a83a38b5
Mikail <mikailsyed@yahoo.com>
2025-01-06 16:22:31 -0500

    Fixed issue

--- vocabWord.py
+++ vocabWord.py
@@ -8,11 +8,14 @@ def main():
     print("1. Enter a new word")
     print("2. Practice words")
     resp = input("Choose either 1 or 2: ")
-    if type(resp) != int or (resp < 1 or resp > 2):
+    if int(resp) == 1:
+        enterNewWords(words)
+    elif int(resp) == 2:
+        print("list words lol")
+    else:
         print("Choose a proper number")
         return
-    if resp == 1:
-        enterNewWords(words)
+    
 
 
 main()
